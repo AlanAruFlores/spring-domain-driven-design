8b3f8e58eb42395bcb9cb7e252bb662ba0cd0382
AlanAruFlores <alanaruquipa242@gmail.com>
2025-03-08 22:19:46 -0300

    first commit

---
+++ src/test/java/ar/com/ddd/ddd_architecture/DddArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ddd.ddd_architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
