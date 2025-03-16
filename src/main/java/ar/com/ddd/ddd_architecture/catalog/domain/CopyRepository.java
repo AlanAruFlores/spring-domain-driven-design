54981a1782ae0249f7a5966f4d139a24de61353e
AlanAruFlores <alanaruquipa242@gmail.com>
2025-03-16 12:56:58 -0300

    defining the repositories of catalog domain

---
+++ src/main/java/ar/com/ddd/ddd_architecture/catalog/domain/CopyRepository.java
@@ -0,0 +1,6 @@
+package ar.com.ddd.ddd_architecture.catalog.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CopyRepository extends CrudRepository<CopyBook, CopyId> {
+}
